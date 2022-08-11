d898d263197c47dc5c64bb1d66e77f39e6537008
Danthemes <danthemes@gmail.com>
2022-08-11 14:17:31 +0300

    done

--- src/App.js
+++ src/App.js
@@ -17,35 +17,47 @@ const App = () => {
             name: 'username',
             type: 'text',
             placeholder: 'Username',
-            label: 'Username'
+            errorMessage: 'Username should be 3-16 characters and shouldn\'t include any spacial characters',
+            label: 'Username',
+            pattern: "^[A-Za-z0-9]{3,16}$",
+            required: true
         },
         {
             id: 2,
             name: 'email',
-            type: 'text',
+            type: 'email',
             placeholder: 'Email',
-            label: 'Email'
+            errorMessage: 'It should a valid email address',
+            label: 'Email',
+            required: true
         },
         {
             id: 3,
             name: 'birthday',
             type: 'date',
             placeholder: 'Birthday',
-            label: 'Birthday'
+            label: 'Birthday',
+            required: true
         },
         {
             id: 4,
             name: 'password',
             type: 'password',
             placeholder: 'Password',
-            label: 'Password'
+            errorMessage: 'Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character',
+            label: 'Password',
+            pattern: "(?=[A-Za-z0-9@#$%^&+!=]+$)^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+!=])(?=.{8,}).*$",
+            required: true
         },
         {
             id: 5,
             name: 'confirmPassword',
             type: 'password',
             placeholder: 'Confirm Password',
-            label: 'Confirm Password'
+            errorMessage: 'Passwords don\'t match',
+            label: 'Confirm Password',
+            pattern: values.password,
+            required: true
         }
     ]
 
@@ -53,7 +65,7 @@ const App = () => {
         e.preventDefault();
 
         const data = new FormData(e.target);
-        console.log(Object.fromEntries(data.entries()));
+        // console.log(Object.fromEntries(data.entries()));
     }
 
     const handleChange = (e) => {
--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './formInput.css';
 
 const FormInput = (props) => {
-    console.log(props);
+  const {label, errorMessage, handleChange, id, ...inputProps} = props;
+  const [focused, setFocused] = useState(false);
+
+  const handleFocus = () => {
+    setFocused(true);
+  }
+
   return (
     <div className="formInput">
-        <label>{props.label}</label>
-        <input placeholder={props.placeholder} name={props.name} type={props.type} onChange={props.handleChange} />
+        <label>{label}</label>
+        <input {...inputProps} onChange={handleChange} onBlur={handleFocus} focused={focused.toString()}
+        onFocus={() => inputProps.name === 'confirmPassword' && setFocused(true) }
+
+        />
+        <span>{errorMessage}</span>
     </div>
   )
 }
